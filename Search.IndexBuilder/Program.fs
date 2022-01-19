9ade2f3c4ee5977c57898c44d548540120aa0f09
takatto <63276803+takattowo@users.noreply.github.com>
2022-01-19 20:45:33 +0700

    Add project files.

---
+++ Search.IndexBuilder/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+open System.Text.Json
+open System.Text.Json.Serialization
+open Search
+open JsonExtensions
+open IndexTools
+open Packager
+
+[<EntryPoint>]
+let main argv =
+    let baseDir = Environment.CurrentDirectory
+    let searchText = File.ReadAllText <| Path.Combine(baseDir, "..", "..", "index.json")
+
+    let options = JsonSerializerOptions()
+    options.PropertyNameCaseInsensitive <- true
+    options.Converters.Add(InvalidDateTimeConverter())
+
+    let searchData = JsonSerializer.Deserialize<SearchData>(searchText, options)
+
+    printfn "Got data from export, there are %d posts" searchData.posts.Length
+
+    cleanupIndex baseDir
+    |> makeIndex searchData
+    |> packageIndex baseDir
+
+    0
---
+++ Search.Site/SearchComponent.fs
@@ -0,0 +1,42 @@
+﻿namespace Search.Site
+
+open Microsoft.AspNetCore.Components
+open Lucene.Net.Index
+open Lucene.Net.Store
+open System.Net.Http
+open System.Threading.Tasks
+open SearchResult
+open IndexTools
+
+module Task =
+    let Ignore(resultTask: Task<_>): Task = upcast resultTask
+
+type SearchComponent() =
+    inherit ComponentBase()
+    let mutable dir: FSDirectory = null
+    let mutable reader: IndexReader = null
+
+    [<Inject>]
+    member val Http: HttpClient = null with get, set
+
+    member val IndexLoaded = false with get, set
+
+    override this.OnInitializedAsync() =
+        task {
+            let! indexPackagePath = downloadIndex this.Http.GetStreamAsync
+            let indexPath = extractZip indexPackagePath
+
+            dir <- FSDirectory.Open indexPath
+            reader <- DirectoryReader.Open dir
+
+            this.IndexLoaded <- true
+        }
+        |> Task.Ignore
+
+    member val SearchTerm = "" with get, set
+    member val SearchResults = Array.empty<Post> with get, set
+
+    member this.Search() =
+        match this.SearchTerm with
+        | "" -> ignore()
+        | term -> this.SearchResults <- search term reader
